a219228fcc6759d2b41b559f688f79e0907ac095
Gage <gagegunn11@gmail.com>
2023-01-13 15:58:59 -0700

    Update Data Handling Method 2.py

--- Data Handling Method 2.py
+++ Data Handling Method 2.py
@@ -10,12 +10,16 @@ from datetime import datetime
 ## Variables ##
 ID = 1
 current_time = datetime.now()
-file_path = r'C:\DHM2\\' + str(ID) + ' ' + current_time.strftime('%m-%d-%Y_%Hhr-%Mmin-%Ssec') + '.txt'
-folder_path = r'C:\DHM2'
+file_path = r'C:\home\\' + str(ID) + ' ' + current_time.strftime('%m-%d-%Y_%Hhr-%Mmin-%Ssec') + '.txt'
+folder_path = r'C:\home'
 stop_char = ' '
 
 
 ## Main ##
+
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
 files_list = [file for file in os.listdir(folder_path) if file.split(stop_char)[0] == str(ID)]
 
 while files_list:
@@ -25,4 +29,4 @@ while files_list:
     
 file_path = r'C:\DHM2\\' + str(ID) + ' ' + current_time.strftime('%m-%d-%Y_%Hhr-%Mmin-%Ssec') + '.txt' #Update the file path to the new ID
 with open(file_path, 'w') as fp:
-    fp.write('This is a test file with ID ' + str(ID))
+    fp.write('This is a test file with ID ' + str(ID)) # <<<<<< Change for
