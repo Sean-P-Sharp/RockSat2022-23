e6b7cb1a64bd2b456d2b1cefe9c02664a348b7de
Gage <gagegunn11@gmail.com>
2023-01-20 16:46:43 -0700

    Update Data Handling Framework.py

--- Data Handling Framework.py
+++ Data Handling Framework.py
@@ -1,11 +1,26 @@
 '''
-RRCC/ACC Rocksat-X 2023
 Data Handling Framework
 
+RRCC/ACC Rocksat-X 2023
+Data Handling Framework
 This file will
 1. Create all necessary folders for data storage
 2. Allow files to be created (named ID [date(m-d-y)]_[time(h-m-s)])
 3. Allow data to be written to said files
+
+-----------------------------------------------------------------------
+
+HOW TO USE
+1. Create Folders - fs.create_folders() 
+2. Create a new file - fs.write_to_file("Folder", "Sub Folder (if none just leave as '')", [value])
+3. Write to the most recently created file - 
+    folder_path = os.path.join(Main File Path, "Folder", "Subfolder") << If no subfolder, remove the last part
+    file_path = highest_id_path[folder_path]
+    with open(file_path, 'a') as file:
+        file.write("\nAdditional data")
+
+*Notes*
+1. Make sure to assign correct file path to create folders to
 '''
 
 
@@ -29,6 +44,7 @@ class FileStructure:
                 for subfolder in self.subfolders[folder]:
                     subpath = os.path.join(path, subfolder)
                     os.makedirs(subpath, exist_ok=True)
+        
 
     def write_to_file(self, folder_name, subfolder_name, data):
         while True:
@@ -42,6 +58,8 @@ class FileStructure:
                     file.write(data)
                 self.ID = 1
                 break
+        
+        find_highest_id(r"C:\Users\Gage\Desktop\Data")
             
     def flight_log(self, flog):
         while True:
@@ -56,11 +74,26 @@ class FileStructure:
                 self.ID = 1
                 break
 
+### Functions ###
+highest_id = 0
+def find_highest_id(path):
+    for item in os.listdir(path):
+        item_path = os.path.join(path, item)
+        if os.path.isdir(item_path):
+            find_highest_id(item_path)
+        else:
+            # Check if the file name starts with an ID
+            id = item.split(" ")[0]
+            if id.isdigit():
+                if fs.ID > highest_id:
+                    highest_id_path[path] = item_path
+
 
 ### Variables ###
+highest_id_path = {}
 stop_char = ' '
 current_time = datetime.now().strftime("%m-%d-%Y_%Hhr-%Mmin-%Ssec")
-folders = ["BME680", "9DOF", "MLX90640", "Aux Camera", "Geiger Counter", "SEN14722", "_Flight Log"]
+folders = ["BME680", "9DOF", "MLX90640", "PTC08", "Geiger Counter", "SEN14722", "_Flight Log", "VL53L1X"]
 subfolders = {
     "BME680": ["Gas", "Humidity", "Pressure", "Temperature"],
     "9DOF": ["Accelerometer", "Magnetometer", "Gyroscope"]
@@ -70,20 +103,29 @@ subfolders = {
 
 ### Main ###
 #Create folders
-file_structure = FileStructure(r"C:\Users\Gage\Desktop\Data", folders, subfolders)
-file_structure.create_folders()
-file_structure.flight_log('Folders have been created\n')
+fs = FileStructure(r"C:\Users\Gage\Desktop\Data", folders, subfolders)
+fs.create_folders()
+fs.flight_log('Folders have been created\n')
 
 
 
-''' Notes
-1. To write to a singular file while a sensor is activated, use a while loop that runs while a sensor is turned on. Close file once sensor is deactivated and break while loop
-   this may be possible on/around line 43 to include it within the file_structure class
-'''
-
-
-#Test
+### Test Section ###
 test_number = str(20)
 test_flog = 'Testing'
-file_structure.write_to_file("BME680", "Temperature", test_number) #Write to a subfolder
-file_structure.write_to_file('MLX90640', '', test_number) #Write to a folder
+fs.write_to_file("BME680", "Temperature", test_number) #Write to a subfolder
+fs.write_to_file('MLX90640', '', test_number) #Write to a folder
+
+
+#Test example for writing to the file with the highest ID in a given folder
+folder_path = os.path.join(r"C:\Users\Gage\Desktop\Data", "MLX90640")
+file_path = highest_id_path[folder_path]
+with open(file_path, 'a') as file:
+    file.write("\nAdditional data")
+    
+
+
+#Changes Needed
+#1. Flight log will say "Folders have been created" every time, change so then it makes a note if folders have already been made
+#2. Make a way to add to the flight log
+#3. Change file name to something without spaces
+
