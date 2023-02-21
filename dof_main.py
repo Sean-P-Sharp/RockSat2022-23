82f524a9a25a3198220f0b5549a0fce653c6417d
Sean-P-Sharp <90068784+Sean-P-Sharp@users.noreply.github.com>
2023-02-21 11:49:06 -0700

    Add files via upload

--- dof_main.py
+++ dof_main.py
@@ -5,14 +5,13 @@ import adafruit_lsm9ds1
 import dof_interface
 import time
 
-cs=digitalio.DigitalInOut(board.D17)
 i2c = board.I2C()
-dof = adafruit_lsm9ds1.LSM9DS1_I2C(i2c, debug=False)
+dof = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
-bme_interface = dof_interface.dof_interface("9DOF", dof, sample_rate=1)
+dof_interface = dof_interface.dof_interface("9DOF", dof, sample_rate=100)
 
-bme_interface.start_data_collection()
+dof_interface.start_data_collection()
 
 time.sleep(20)
 
-bme_interface.stop_data_collection()
+dof_interface.stop_data_collection()
