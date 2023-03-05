75eff63f62ff673fbb36cd0e4d56060b6b18a9a4
Sean-P-Sharp <90068784+Sean-P-Sharp@users.noreply.github.com>
2023-03-05 14:32:52 -0700

    Update SEN14722_DistanceSensor.py

--- SEN14722_DistanceSensor.py
+++ SEN14722_DistanceSensor.py
@@ -1,26 +1,6 @@
-#import sensor_interface
 import time
 import sys
 import qwiic_vl53l1x
-
-#i2c = board.I2C()
-#SEN_interface = qwiic_vl53l1x.QwiicVL53L1X(i2c)
-
-#class SEN14722_interface(sensor_interface.sensor_interface):
- #   def __init__(self,name, SEN14722_object, csv_writer=None, sample_rate=3):
-  #      super().__init__(name, SEN14722_object, csv_writer=csv_writer, sample_rate=sample_rate)
-#def collect_data(self):
- #   distance_sensor = self.sensor_obj
-  #  distance_sensor.begin()
-
- #   while not self.stop_thread:
-
-  #      distance_mm = distance_sensor.get_distance()
-   #     distance_cm = distance_mm/10.0
-    #    self.store_distance_as_csv(distance_cm)
-
-     #   time.sleep(self.sample_rate)
-      #  distance_sensor.end()
 
 def runExample():
     print("\nSparkFun VL53L1X Example 1\n")
@@ -45,4 +25,3 @@ def runExample():
 
         except Exception as e:
             print(e)
-
